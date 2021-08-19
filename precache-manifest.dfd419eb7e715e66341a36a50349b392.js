36822bf53a652de5d16091b88b60c22c17694356
Cássio Pereira <cassiorodp@gmail.com>
2021-08-19 12:17:53 -0300

    Updates

---
+++ precache-manifest.dfd419eb7e715e66341a36a50349b392.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ab410eb3506b194ac8af52cd5245ba",
+    "url": "/buy-please/index.html"
+  },
+  {
+    "revision": "46a60db756992cd5ff22",
+    "url": "/buy-please/static/css/main.86df52fa.chunk.css"
+  },
+  {
+    "revision": "0e4870ec6437d96318fa",
+    "url": "/buy-please/static/js/2.b04af1ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/buy-please/static/js/2.b04af1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a60db756992cd5ff22",
+    "url": "/buy-please/static/js/main.6dc66c0a.chunk.js"
+  },
+  {
+    "revision": "5ce125dc602a34a13781",
+    "url": "/buy-please/static/js/runtime-main.369ce355.js"
+  },
+  {
+    "revision": "2a687ed98902ea3e7d2410f75383af74",
+    "url": "/buy-please/static/media/icone-adicionar.2a687ed9.svg"
+  },
+  {
+    "revision": "542b3133be0ee0cb13f1f47f2ef9f546",
+    "url": "/buy-please/static/media/icone-cabeçalho.542b3133.svg"
+  },
+  {
+    "revision": "2253c48fe04285d73c8944e999553036",
+    "url": "/buy-please/static/media/icone-deletar.2253c48f.svg"
+  },
+  {
+    "revision": "200ec5b0fc18cd1737c3a47e95f26ecd",
+    "url": "/buy-please/static/media/icone-pesquisa.200ec5b0.svg"
+  },
+  {
+    "revision": "ec6a7ea22cedbbf1f896de1d35fd3e70",
+    "url": "/buy-please/static/media/icone-produtos.ec6a7ea2.svg"
+  },
+  {
+    "revision": "8440f26beb969cae9c6e7f002ab8c151",
+    "url": "/buy-please/static/media/icone-remover.8440f26b.svg"
+  },
+  {
+    "revision": "a1af57c30a5973b6083b70b3566d7abb",
+    "url": "/buy-please/static/media/icone-voltar.a1af57c3.svg"
+  }
+]);
